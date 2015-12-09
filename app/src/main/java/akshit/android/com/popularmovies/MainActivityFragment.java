32785f6b763cd8f99b21794f69af8f5b03b27c55
Akshit Gupta <akshit.gupta@snapdeal.com>
2015-12-09 21:37:42 +0530

    Async task setup

--- app/src/main/java/akshit/android/com/popularmovies/MainActivityFragment.java
+++ app/src/main/java/akshit/android/com/popularmovies/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package akshit.android.com.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.util.Log;
+
 import java.util.Arrays;
 import java.util.Date;
 
@@ -16,6 +18,7 @@ import java.util.Date;
 public class MainActivityFragment extends Fragment {
 
     private MovieAdapter movieAdapter;
+    public static String TAG = MainActivityFragment.class.getSimpleName();
 
     Movie[] movies = {
             new Movie("Captain America", "It has captain america", "5", new Date()),
@@ -27,21 +30,50 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        fetchMoviePosterTask();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
         GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
-
-        Log.i("MainActivityFragment","Movies [0]"+movies[0].plotSummary+movies[0].title);
-
+        Log.i("MainActivityFragment", "Movies [0]" + movies[0].plotSummary + movies[0].title);
         movieAdapter = new MovieAdapter(getActivity(), Arrays.asList(movies));
-
         gridView.setAdapter(movieAdapter);
 
         return rootView;
+
+    }
+
+    public void fetchMoviePosterTask() {
+
+    }
+
+    public class FetchMoviePosterTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "Inside onPreExecute Method");
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            return new String[0];
+        }
+
+        @Override
+        protected void onPostExecute(String[] data) {
+            Log.i(TAG, "Inside onPostExecute method");
+
+            super.onPostExecute(data);
+        }
 
     }
 }
