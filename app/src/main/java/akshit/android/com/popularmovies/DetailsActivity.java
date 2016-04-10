794858de48977843d5dcb7606316f4a6812703b5
Akshit Gupta <akshitonline@gmail.com>
2016-04-10 10:45:58 +0530

    UI refracting and button for facurite

--- app/src/main/java/akshit/android/com/popularmovies/DetailsActivity.java
+++ app/src/main/java/akshit/android/com/popularmovies/DetailsActivity.java
@@ -33,14 +33,10 @@ public class DetailsActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().add(
                     android.R.id.content, details).commit();
         }
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+        // For button
+
     }
 
     public static class DetailsActivityFragment extends Fragment {
